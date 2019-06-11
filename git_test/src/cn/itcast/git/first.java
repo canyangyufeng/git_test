e7a53172a7615491909cefb120d18637f9a4928e
wuliji <1075623099@qq.com>
2019-06-11 19:23:08 +0800

    添加了注释以及一个新的测试方法

--- git_test/src/cn/itcast/git/first.java
+++ git_test/src/cn/itcast/git/first.java
@@ -5,11 +5,14 @@ public class first {
         System.out.println("hello,git progect");
         System.out.println("hello,git progect");
         System.out.println("hello,git progect");
-
+        //这是一行注释
 
     }
     public void test(){
         int i = 0 ;
         i+=10;
+    }
+    public void test2(){
+        System.out.println("这是一行测试代码");
     }
 }
