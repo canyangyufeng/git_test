6de9d817fdfe31bdde45b235311a2f62190feedc
wuliji <1075623099@qq.com>
2019-06-11 18:32:46 +0800

    把本地工程添加到git

---
+++ git_test/src/cn/itcast/git/first.java
@@ -0,0 +1,11 @@
+package cn.itcast.git;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hello,git progect");
+        System.out.println("hello,git progect");
+        System.out.println("hello,git progect");
+
+
+    }
+}
