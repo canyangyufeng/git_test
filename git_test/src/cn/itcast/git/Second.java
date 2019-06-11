185d08b9cd867738db89391d11e0c196468d46f0
wuliji <1075623099@qq.com>
2019-06-11 20:12:15 +0800

    添加分支dev,添加测试类以及junit单元测试jar包

--- git_test/src/cn/itcast/git/Second.java
+++ git_test/src/cn/itcast/git/Second.java
@@ -1,6 +1,9 @@
 package cn.itcast.git;
 
+import org.junit.Test;
+
 public class Second {
+    @Test
     public void test(){
         System.out.println("1234，1234像首歌");
         System.out.println("绿色军营，绿色军营教会我");
