18ce3fd5f68ba7fa9eaeee8ba29f87cf9e23a451
wuliji <1075623099@qq.com>
2019-06-11 19:33:17 +0800

    修改类名以及添加了Second类中的test方法

--- git_test/src/cn/itcast/git/First.java
+++ git_test/src/cn/itcast/git/First.java
@@ -1,6 +1,6 @@
 package cn.itcast.git;
 
-public class first {
+public class First {
     public static void main(String[] args) {
         System.out.println("hello,git progect");
         System.out.println("hello,git progect");
---
+++ git_test/src/cn/itcast/git/Second.java
@@ -0,0 +1,10 @@
+package cn.itcast.git;
+
+public class Second {
+    public void test(){
+        System.out.println("1234，1234像首歌");
+        System.out.println("绿色军营，绿色军营教会我");
+        System.out.println("唱的山摇地也动");
+        System.out.println("唱的花开水欢乐");
+    }
+}
